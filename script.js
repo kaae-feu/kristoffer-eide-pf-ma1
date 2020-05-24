e9898093355a3e8a687bf48af8079f9cb0bcfc73
kaae-feu <63543519+kaae-feu@users.noreply.github.com>
2020-05-24 22:10:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// question 1
+
+// var firstVar;
+
+// question 2
+
+// var name = "kristoffer";
+
+// question 3
+
+// var number = 1;
+
+// question 4
+
+// var division = 20 / 5;
+
+// question 5
+
+// typeof frog;
+
+// question 6
+
+// var orderHasShipped = true;
+
+// question 7
+
+// if(orderHasShipped === true) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+// question 8
+
+// for(var i = 0; i < 10; i++) {
+    console.log(i);
+}
